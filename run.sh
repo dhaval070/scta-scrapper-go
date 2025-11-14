099763c6fe02a39d26c66062d24ce3df757114a9
dhaval dhanani <dhaval070@gmail.com>
2025-11-14 22:19:03 +0530

    removed run-all.sh , updated run.sh

--- run.sh
+++ run.sh
@@ -1,36 +1,70 @@
 #!/bin/bash
-# usage: sh run.sh alliancehockey 2024-12-01
-# date is optional, if not provided then current date is used
-dt="$2"
+# usage: sh run-all.sh <sites> [date]
+# sites: mandatory - can be --all or comma-separated list (e.g., site1,site2,site3)
+# date: optional - defaults to current date (format: YYYY-MM-DD)
+# Examples:
+#   sh run-all.sh --all              # all sites, current date
+#   sh run-all.sh site1,site2        # specific sites, current date
+#   sh run-all.sh --all 2024-12-01   # all sites, specific date
+#   sh run-all.sh site1 2024-12-01   # specific site, specific date
 
-if [ -z $dt ]; then
+# Sites is mandatory
+sites="$1"
+if [ -z "$sites" ]; then
+    echo "Error: sites parameter is required"
+    echo "Usage: sh run-all.sh <sites> [date]"
+    echo "  sites: --all or comma-separated list (e.g., site1,site2,site3)"
+    echo "  date: optional, defaults to current date (YYYY-MM-DD)"
+    exit 1
+fi
+
+if [ "$sites" != "--all" ]; then
+    sites="--sites $sites"
+fi
+# Date is optional, defaults to current date
+dt="$2"
+if [ -z "$dt" ]; then
     dt=`date +%Y-%m-%d`
 fi
 
 sd=`echo $dt|sed 's/-//g'`
+
+dt=`date +%Y-%m-%d`
 dir="var/$sd"
 d1="$dir/with-surface"
-
-mkdir -p $d1
-site="$1"
 
 yyyy=`echo $sd|cut -c 1-4`
 mm=`echo $sd|cut -c 5-6`
 
 mmyyyy="$mm$yyyy"
-echo "$site $mmyyyy"
 
-# Use new universal scraper instead of individual site binary
-./scraper --sites=$site --import-locations --date $mmyyyy --outfile /tmp/$site.csv
+mkdir -p $d1
+
+# Create unique temporary directory
+tmpdir=$(mktemp -d /tmp/calendar-scraper.XXXXXX)
+
+# Use new universal scraper instead of individual site binaries
+echo "Running universal scraper for sites: $sites with date: $mmyyyy"
+
+./scraper $sites --import-locations --date $mmyyyy --outfile $tmpdir/schedules.csv
 
 if [ $? -ne 0 ]; then
-    echo "$site failed"
-    exit
+    echo "Scraper failed"
+    exit 1
 fi
 
-if [ -e /tmp/${site}_$site.csv -a -s /tmp/${site}_$site.csv ]; then
-    f="$dir/$site.csv"
-    csvtool --encoding utf8 -c 1-6 /tmp/${site}_$site.csv > $f
-    
-    go run ./cmd/site-schedule/main.go -site $site -infile $f > $d1/$site.csv --import -cutoffdate $dt &
-fi
+# Process each site's output file
+for csv_file in $tmpdir/*_schedules.csv; do
+    if [ -e "$csv_file" ]; then
+        site=$(basename "$csv_file" _schedules.csv)
+        echo "Processing $site"
+
+        f="$dir/$site.csv"
+        csvtool --encoding utf8 -c 1-6 "$csv_file" > $f
+
+        go run ./cmd/site-schedule/main.go -site $site -infile $f > $d1/$site.csv --import -cutoffdate $dt &
+    fi
+done
+
+# Clean up temporary directory
+rm -rf $tmpdir
