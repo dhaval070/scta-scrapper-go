3e304b5b40920860b7f181635f1ebafc05bd708e
dhaval dhanani <dhaval070@gmail.com>
2025-12-06 15:01:27 +0530

    updated venue import to insert in batches to improve speed

--- pkg/repository/master.go
+++ pkg/repository/master.go
@@ -10,130 +10,162 @@ import (
 )
 
 func (r *Repository) MasterImportJson(js []entity.JsonLocation) {
+	batchSize := 100
 	err := r.DB.Transaction(func(tx *gorm.DB) (err error) {
+		provinces := make(map[int32]model.Province)
+		locations := []model.Location{}
+		surfaces := []model.Surface{}
+		feedModes := make(map[int32]model.FeedMode)
+		surfaceFeedModes := []model.SurfaceFeedMode{}
+		renditions := []model.Rendition{}
+
 		for _, loc := range js {
 			log.Println(loc.ID)
-			err = r.MasterImportLoc(tx, loc)
-			if err != nil {
+
+			// Collect provinces
+			if _, exists := provinces[loc.Province.ID]; !exists {
+				provinces[loc.Province.ID] = model.Province{
+					ID:           loc.Province.ID,
+					ProvinceName: loc.Province.Name,
+				}
+			}
+
+			// Build location
+			location := model.Location{
+				ID:                  loc.ID,
+				Address1:            loc.Address1,
+				Address2:            loc.Address2,
+				City:                loc.City,
+				Name:                loc.Name,
+				UUID:                loc.UUID,
+				RecordingHoursLocal: loc.RecordingHoursLocal,
+				PostalCode:          loc.PostalCode,
+				AllSheetsCount:      loc.AllSheetsCount,
+				Longitude:           loc.Longitude,
+				Latitude:            loc.Latitude,
+				TotalSurfaces:       int32(len(loc.Surfaces)),
+				ProvinceID:          loc.Province.ID,
+				VenueStatus:         loc.VenueStatus["name"].(string),
+				Zone:                loc.Zone["name"].(string),
+			}
+			b, _ := json.Marshal(loc.LogoURL)
+			location.LogoURL = string(b)
+			locations = append(locations, location)
+
+			// Build surfaces
+			for _, surface := range loc.Surfaces {
+				s := model.Surface{
+					ID:         surface.ID,
+					LocationID: loc.ID,
+					Name:       surface.Name,
+					UUID:       surface.UUID,
+					OrderIndex: surface.OrderIndex,
+					VenueID:    surface.VenueID,
+					ClosedFrom: surface.ClosedFrom,
+					ComingSoon: surface.ComingSoon,
+					Online:     surface.Online,
+					Status:     surface.Status["name"].(string),
+				}
+				if len(surface.Sports) > 0 {
+					s.Sports = surface.Sports[0]["name"].(string)
+				}
+				s.FirstMediaDate = surface.FirstMedia.FirstMediaDate
+				surfaces = append(surfaces, s)
+
+				// Collect feed modes
+				for _, fm := range surface.FeedModes {
+					if _, exists := feedModes[fm.ID]; !exists {
+						feedModes[fm.ID] = model.FeedMode{
+							ID:       fm.ID,
+							FeedMode: fm.Name,
+						}
+					}
+					surfaceFeedModes = append(surfaceFeedModes, model.SurfaceFeedMode{
+						SurfaceID:  surface.ID,
+						FeedModeID: fm.ID,
+					})
+				}
+
+				// Collect renditions
+				for _, rendition := range surface.Renditions {
+					renditions = append(renditions, model.Rendition{
+						ID:        int32(rendition.ID),
+						SurfaceID: surface.ID,
+						Name:      rendition.Name,
+						Width:     int32(rendition.Width),
+						Height:    int32(rendition.Height),
+						Ratio:     rendition.Ratio,
+						Bitrate:   rendition.Bitrate,
+					})
+				}
+			}
+		}
+
+		// Batch insert/update provinces
+		provinceSlice := make([]model.Province, 0, len(provinces))
+		for _, p := range provinces {
+			provinceSlice = append(provinceSlice, p)
+		}
+		if len(provinceSlice) > 0 {
+			if err = tx.Save(&provinceSlice).Error; err != nil {
 				return err
 			}
 		}
+
+		// Batch insert/update locations
+		for i := 0; i < len(locations); i += batchSize {
+			end := i + batchSize
+			end = min(end, len(locations))
+			batch := locations[i:end]
+			if err = tx.Save(&batch).Error; err != nil {
+				return err
+			}
+		}
+
+		// Batch insert/update surfaces
+		for i := 0; i < len(surfaces); i += batchSize {
+			end := i + batchSize
+			end = min(end, len(surfaces))
+			batch := surfaces[i:end]
+			if err = tx.Save(&batch).Error; err != nil {
+				return err
+			}
+		}
+
+		// Batch insert/update feed modes
+		feedModeSlice := make([]model.FeedMode, 0, len(feedModes))
+		for _, fm := range feedModes {
+			feedModeSlice = append(feedModeSlice, fm)
+		}
+		if len(feedModeSlice) > 0 {
+			if err = tx.Save(&feedModeSlice).Error; err != nil {
+				return err
+			}
+		}
+
+		// Batch insert/update surface feed modes
+		for i := 0; i < len(surfaceFeedModes); i += batchSize {
+			end := i + batchSize
+			end = min(end, len(surfaceFeedModes))
+			batch := surfaceFeedModes[i:end]
+			if err = tx.Save(&batch).Error; err != nil {
+				return err
+			}
+		}
+
+		// Batch insert/update renditions
+		for i := 0; i < len(renditions); i += batchSize {
+			end := i + batchSize
+			end = min(end, len(renditions))
+			batch := renditions[i:end]
+			if err = tx.Save(&batch).Error; err != nil {
+				return err
+			}
+		}
+
 		return err
 	})
 	if err != nil {
 		panic(err.Error())
 	}
-}
-
-func (r *Repository) MasterImportLoc(db *gorm.DB, l entity.JsonLocation) error {
-	var err error
-	loc := model.Location{
-		ID:                  l.ID,
-		Address1:            l.Address1,
-		Address2:            l.Address2,
-		City:                l.City,
-		Name:                l.Name,
-		UUID:                l.UUID,
-		RecordingHoursLocal: l.RecordingHoursLocal,
-		PostalCode:          l.PostalCode,
-		AllSheetsCount:      l.AllSheetsCount,
-		Longitude:           l.Longitude,
-		Latitude:            l.Latitude,
-		TotalSurfaces:       int32(len(l.Surfaces)),
-	}
-	b, _ := json.Marshal(l.LogoURL)
-	loc.LogoURL = string(b)
-
-	province := model.Province{ID: l.Province.ID}
-
-	// import all for RAMP locations. ramp locations are not limited to Ontario
-	// if l.Province.Name != "Ontario" {
-	// 	return nil
-	// }
-
-	if err = db.First(&province).Error; err != nil {
-		province.ProvinceName = l.Province.Name
-		if err = db.Save(&province).Error; err != nil {
-			return err
-		}
-	}
-	loc.ProvinceID = l.Province.ID
-	loc.VenueStatus = l.VenueStatus["name"].(string)
-	loc.Zone = l.Zone["name"].(string)
-
-	if err = db.Save(&loc).Error; err != nil {
-		return err
-	}
-
-	for _, surface := range l.Surfaces {
-		err = r.MasterImportSurface(db, l.ID, surface)
-		if err != nil {
-			return err
-		}
-	}
-
-	// Recalculate total_surfaces from actual database to ensure accuracy
-	var count int64
-	if err = db.Model(&model.Surface{}).Where("location_id = ?", l.ID).Count(&count).Error; err != nil {
-		return err
-	}
-	if err = db.Model(&model.Location{}).Where("id = ?", l.ID).Update("total_surfaces", count).Error; err != nil {
-		return err
-	}
-
-	return err
-}
-
-func (r *Repository) MasterImportSurface(db *gorm.DB, locId int32, surface entity.JsonSurface) (err error) {
-	s := model.Surface{
-		ID:         surface.ID,
-		LocationID: locId,
-		Name:       surface.Name,
-		UUID:       surface.UUID,
-		OrderIndex: surface.OrderIndex,
-		VenueID:    surface.VenueID,
-		ClosedFrom: surface.ClosedFrom,
-		ComingSoon: surface.ComingSoon,
-		Online:     surface.Online,
-	}
-	s.Status = surface.Status["name"].(string)
-
-	if len(surface.Sports) > 0 {
-		s.Sports = surface.Sports[0]["name"].(string)
-	}
-
-	s.FirstMediaDate = surface.FirstMedia.FirstMediaDate
-	if err = db.Save(&s).Error; err != nil {
-		return err
-	}
-
-	for _, fm := range surface.FeedModes {
-		if err = db.Save(&model.FeedMode{
-			ID:       fm.ID,
-			FeedMode: fm.Name,
-		}).Error; err != nil {
-			return err
-		}
-
-		db.Save(&model.SurfaceFeedMode{
-			SurfaceID:  surface.ID,
-			FeedModeID: fm.ID,
-		})
-	}
-
-	for _, rendition := range surface.Renditions {
-		if err = db.Save(&model.Rendition{
-			ID:        int32(rendition.ID),
-			SurfaceID: surface.ID,
-			Name:      rendition.Name,
-			Width:     int32(rendition.Width),
-			Height:    int32(rendition.Height),
-			Ratio:     rendition.Ratio,
-			Bitrate:   rendition.Bitrate,
-		}).Error; err != nil {
-			return err
-		}
-	}
-
-	return err
 }
