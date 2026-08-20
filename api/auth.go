f5a5839c39457f368b0a4481b250854827e84d36
dhaval dhanani <dhaval070@gmail.com>
2026-08-21 00:30:48 +0530

    fixed auth

--- api/auth.go
+++ api/auth.go
@@ -85,7 +85,7 @@ func (app *App) AuthMiddleware() gin.HandlerFunc {
 		defer s.SessionRelease(c.Writer)
 
 		url := c.Request.URL.String()
-		if !strings.HasPrefix(url, "/v1/") && url != "/swagger/" && url != "/login" && url != "/logout" {
+		if !strings.HasPrefix(url, "/v1/") && url != "/swagger/" && url != "/login" && url != "/logout" && url != "/session" {
 			if s.Get("username") == nil {
 				log.Println("url: ", url)
 				c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
